6ae215f99a4f8dbf106ef4f99035b10350e066ec
mcuevasb <surasiterix@users.noreply.github.com>
2019-08-06 16:32:06 -0300

    Forzando la instalación de pip
    Se incluyó la instalación de python-pip

--- setup_tools/install_ansible.sh
+++ setup_tools/install_ansible.sh
@@ -2,10 +2,10 @@
 python --version
 echo "apt-get -qq update"
 sudo apt-get -qq update
-echo "apt-get -qq -y install python-dev libssl-dev libffi-dev"
-sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
-echo "pip2 install pycrypto pyyaml ansible --quiet"
-sudo pip2 install pycrypto pyyaml ansible --quiet
+echo "apt-get -qq -y install python-dev python-pip libssl-dev libffi-dev"
+sudo apt-get -qq -y install python-dev python-pip libssl-dev libffi-dev
+echo "pip install pycrypto pyyaml ansible --quiet"
+sudo pip install pycrypto pyyaml ansible --quiet
 
 echo "apt-get -qq clean"
 sudo apt-get -qq clean
